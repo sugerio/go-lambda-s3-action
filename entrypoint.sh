73059af1cee14d2c95eba13582a645dee8b34a79
chengjun <chengjun@suger.io>
2022-02-20 20:33:37 -0800

    add auto update lambda

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,6 +18,14 @@ aws --region $AWS_DEFAULT_REGION s3 cp ../artifacts/$RELEASE_ZIP $S3_RELEASE_ZIP
 aws --region $AWS_DEFAULT_REGION s3 cp $S3_RELEASE_ZIP $S3_LATEST_ZIP --no-progress
 echo "Successfully uploaded zip to $S3_RELEASE_ZIP and $S3_LATEST_ZIP"
 
+# if the LAMBDA_FUNCTION_NAME is not empty
+if [ ! -z "$LAMBDA_FUNCTION_NAME" ] then
+    echo "Update lambda function" $LAMBDA_FUNCTION_NAME "from s3 bucket"
+    aws --region $AWS_DEFAULT_REGION lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME \
+    --s3-bucket $AWS_BUCKET_NAME --s3-key $AWS_BUCKET_KEY_PREFIX/$LATEST_ZIP --publish true
+    echo "Successfully updated lambda function" $LAMBDA_FUNCTION_NAME "from s3 bucket"
+fi
+
 cd ../../
 rm -rf bin/
-echo "Successfully remove the bin folder"
+echo "Successfully removed the bin folder"
